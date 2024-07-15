55f66e0ff9b7a3e0c4a828fa82658033c7a23991
Ryan Arcel Galendez <ryanargee2@gmail.com>
2024-07-15 21:36:40 +0800

    initial commit

---
+++ PiniaPractice/src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <div class="count">
+      {{ storeCounter.count }}
+    </div>
+    <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+    </div>
+    <hr />
+    <div>This number is: {{ oddOrEven }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+
+const storeCounter = useCounterStore()
+const count = ref(0)
+
+const increaseCount = () => {
+  count.value++
+}
+
+const decreaseCount = () => {
+  count.value--
+}
+
+const oddOrEven = computed(() => {
+  return count.value % 2 === 0 ? 'Even' : 'Odd'
+})
+</script>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
